f1511ae07a0d219e3aff5d85a39653bbe6faf081
Bruce Brown <yandbdevelopment@gmail.com>
2025-10-25 06:01:54 -0400

    file fix? guess i saved it as plain text XD

---
+++ password generator.js
@@ -0,0 +1,13 @@
+//function generatees psuedo random password
+function generatePassword(length) {
+
+   let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+console.log(generatePassword(5));
